cdef0bcbb136da81a7b2682db327bf96f4bdb160
Cody Rivera <cjrivera1@crimson.ua.edu>
2020-09-11 22:47:15 -0500

    Add Haskell practice

---
+++ practice/haskell/Borie.hs
@@ -0,0 +1,128 @@
+-- |
+-- Exercises directly suggested by Richard Borie
+-- http://cs403.cs.ua.edu/fall2020/exercises.htm
+--
+-- Solved by Cody Rivera, Fall 2020
+module Borie where
+
+import Prelude
+
+-- | #1. Exponentiation
+power :: (Num m, Integral n) => m -> n -> m
+power _ 0 = 1
+power m n
+  | odd n = m * power m (n - 1)
+  | otherwise = s * s
+  where
+    s = power m (n `div` 2)
+
+-- | #2. Logarithm
+blog :: (Ord m, Num m, Integral n) => m -> m -> n
+blog m q = logHelp m q 0
+  where
+    logHelp m q n
+      | m `power` n >= q = n
+      | otherwise = logHelp m q (n + 1)
+
+-- | #3. Combinations
+comb :: (Integral n) => n -> n -> n
+comb 0 _ = 1
+comb _ 0 = 1
+comb n k
+  | n == k = 1
+  | otherwise = comb (n - 1) k + comb (n - 1) (k - 1)
+
+-- | Ordered list insert
+insertList :: (Ord n) => n -> [n] -> [n]
+insertList n [] = [n]
+insertList n (x : xs)
+  | n <= x = n : (x : xs)
+  | otherwise = x : (insertList n xs)
+
+-- | #4. Insertion sort
+insertionSort :: (Ord n) => [n] -> [n]
+insertionSort [] = []
+insertionSort (x : xs) = insertList x (insertionSort xs)
+
+-- | #5. Selection sort
+selectionSort :: (Ord n) => [n] -> [n]
+selectionSort [] = []
+selectionSort xs = e : (selectionSort $ filter (/= e) xs)
+  where
+    e = minimum xs
+
+-- | Merge
+merge :: (Ord n) => [n] -> [n] -> [n]
+merge [] [] = []
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys)
+  | x <= y = x : (merge xs (y : ys))
+  | otherwise = y : (merge (x : xs) ys)
+
+-- | #6. Merge sort
+mergeSort :: (Ord n) => [n] -> [n]
+mergeSort [] = []
+mergeSort [x] = [x] -- Edge case beware
+mergeSort xs = merge (mergeSort ls) (mergeSort rs)
+  where
+    (ls, rs) = splitAt (l `div` 2) xs
+    l = length xs
+
+-- | Quick sort pivot - deterministic
+quickSortPivot :: [n] -> (n, [n])
+quickSortPivot (x : xs) = (x, xs)
+
+-- | #7. Quick sort
+quickSort :: (Ord n) => [n] -> [n]
+quickSort [] = []
+quickSort xs = quickSort ls ++ [p] ++ quickSort rs
+  where
+    (p, xr) = quickSortPivot xs
+    ls = [l | l <- xr, l <= p]
+    rs = [r | r <- xr, r > p]
+
+-- | BST Definition
+data Tree a = Nil | Node a (Tree a) (Tree a)
+  deriving (Show)
+
+-- | #8. BST membership
+member :: (Ord a) => a -> Tree a -> Bool
+member _ Nil = False
+member a (Node k l r)
+  | a == k = True
+  | a < k = member a l
+  | otherwise = member a r
+
+-- | #9. BST insert
+insert :: (Ord a) => a -> Tree a -> Tree a
+insert a Nil = Node a Nil Nil
+insert a (Node k l r)
+  | a == k = Node k l r
+  | a < k = Node k (insert a l) r
+  | otherwise = Node k l (insert a r)
+
+-- | BST max
+bstMax :: (Ord a) => Tree a -> a
+bstMax (Node a _ Nil) = a
+bstMax (Node a _ r) = bstMax r
+
+-- | #10. BST delete
+delete :: (Ord a) => a -> Tree a -> Tree a
+delete _ Nil = Nil
+delete a (Node k l r)
+  | a == k = help l r
+  | a < k = Node k (delete a l) r
+  | otherwise = Node k l (delete a r)
+  where
+    help Nil Nil = Nil
+    help l Nil = l
+    help Nil r = r
+    help l r = Node m (delete m l) r
+      where
+        m = bstMax l
+
+-- | apply each
+applyEach :: [(a -> b)] -> [a] -> [b]
+applyEach [] _ = []
+applyEach (f : fs) (x : xs) = f x : applyEach fs xs
---
+++ practice/haskell/OldExams.hs
@@ -0,0 +1,112 @@
+-- |
+-- Exams given for CS 403 in past semesters
+-- http://cs403.cs.ua.edu
+--
+-- Solved by Cody Rivera, Fall 2020
+module OldExams where
+
+import Prelude
+
+-- Spring 2015
+
+-- | #1. Convert
+convert :: ([a], [a], [a]) -> [(a, a, a)]
+convert (x : xs, y : ys, z : zs) = (x, y, z) : convert (xs, ys, zs)
+convert _ = [] -- Fallthrough case - length of shortest list
+
+-- | #2. Invert
+invert :: [(a, a, a)] -> ([a], [a], [a])
+invert [] = ([], [], [])
+invert (p : ps) = (x : xs, y : ys, z : zs)
+  where
+    (x, y, z) = p
+    (xs, ys, zs) = invert ps
+
+-- | #3. Infinite list find
+find :: (Ord a) => a -> [a] -> Bool
+find _ [] = False
+find v (x : xs)
+  | v == x = True
+  | v > x = find v xs
+  | otherwise = False
+
+-- | #4. Split range
+splitRange :: (Ord a) => a -> a -> [a] -> ([a], [a])
+splitRange _ _ [] = ([], [])
+splitRange x y (z : zs)
+  | x <= z && z <= y = ((z : is), es)
+  | otherwise = (is, (z : es))
+  where
+    (is, es) = splitRange x y zs
+
+-- | #5. Repeated application
+loop :: (Integral b) => (a -> a) -> a -> b -> a
+loop _ s 0 = s
+loop f s n = loop f (f s) (n - 1)
+
+-- * #6. Redevelop arithmetic on positive Integrals
+
+inc :: Num a => a -> a
+inc x = x + 1
+
+addHelp :: (Integral a) => a -> a -> a -> a
+addHelp cy y s
+  | cy < y = addHelp (inc cy) y (inc s)
+  | otherwise = s
+
+add :: (Integral a) => a -> a -> a
+add x y = addHelp 0 y x
+
+multHelp :: (Integral a) => a -> a -> a -> a -> a
+multHelp x cy y s
+  | cy < y = multHelp x (inc cy) y (add s x)
+  | otherwise = s
+
+mult :: (Integral a) => a -> a -> a
+mult x 0 = 0
+mult x y = multHelp x 0 y 0
+
+-- Ad nauseum
+
+-- | #7. Extremes
+extremes :: [[a]] -> [[a]]
+extremes [] = []
+extremes ((e : es) : xs) = [e, last es] : extremes xs
+
+-- * #8. Curry and uncurry
+
+-- | Curry
+curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
+curry3 f a b c = f (a, b, c)
+
+-- | Uncurry
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f t = f a b c
+  where
+    (a, b, c) = t
+
+-- | Expression Type
+data Expr
+  = BinaryOp (Int -> Int -> Int) Expr Expr
+  | UnaryOp (Int -> Int) Expr
+  | Value Int
+
+-- | #9. Expression Evaluate
+eval :: Expr -> Int
+eval (Value v) = v
+eval (UnaryOp f x) = f (eval x)
+eval (BinaryOp f x y) = f (eval x) (eval y)
+
+-- | #10. Suffixes
+suffixes :: [a] -> [[a]]
+suffixes xs = reverse (help xs)
+  where
+    help [] = [[]]
+    help (x : xs) = (x : xs) : help xs
+
+-- | #11. Prefixes
+prefixes :: [a] -> [[a]]
+prefixes [] = []
+prefixes (x : xs) = [] : map (x :) (prefixes xs)
+
+-- * #12 omitted
