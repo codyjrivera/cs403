3e962385cdfef491b477ad4b8f05bc3f22e5b905
Cody Rivera <cjrivera1@crimson.ua.edu>
2020-10-02 10:48:22 -0500

    Mark Problem 3 as an incorrect solution

--- exams/haskell/problem3.hs
+++ exams/haskell/problem3.hs
@@ -1,8 +1,18 @@
 -- You may modify the given parameters of a function to use pattern matching.
 
-subseq' [] _ = []
+-- Incorrect solution -- this solution assumes f is strictly increasing
+{- subseq' [] _ = []
 subseq' ((xi, x) : xs) (i : is)
   | xi == i = x : subseq' xs is
   | otherwise = subseq' xs (i : is)
 
-subseq f xs = subseq' (zip [0 ..] xs) (map f [0 ..])
+subseq f xs = subseq' (zip [0 ..] xs) (map f [0 ..]) -}
+
+-- I place the instructor's solution here, which works generally (at O(n^2) complexity),
+-- see http://cs403.cs.ua.edu/fall2020/exam2key.pdf
+subseq f xs = help 0
+  where
+    help k =
+      if (f k < 0) || null (drop (f k) xs)
+        then []
+        else (xs !! f k) : help (k + 1)
