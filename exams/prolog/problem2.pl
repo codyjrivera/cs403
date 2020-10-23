050a285e9922c20f8838b67003ba64b92a417667
Cody Rivera <cjrivera1@crimson.ua.edu>
2020-10-23 10:45:38 -0500

    Fix problem with problem2

--- exams/prolog/problem2.pl
+++ exams/prolog/problem2.pl
@@ -1,6 +1,8 @@
 
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
-shared_grandparent(G, X, Y) :- grandparent(G, X), grandparent(G, Y).
+shared_grandparent(G, X, Y) :- grandparent(G, X), grandparent(G, Y).    
+sibling(X, Y) :- parent(A, X), parent(A, Y), X \= Y.
 
-double_cousin(X, Y) :- setof(G, (shared_grandparent(G, X, Y), X \= Y), S),
+% bugfix siblings can not be double cousins now
+double_cousin(X, Y) :- setof(G, (shared_grandparent(G, X, Y), X \= Y, \+(sibling(X, Y))), S),
                        length(S, 4).
